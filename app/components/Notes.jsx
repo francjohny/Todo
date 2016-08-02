1e2829cf391b8c22848ec92226f735d5ee171ed8
Francis <francis@qburst.com>
2016-08-02 16:02:51 +0530

    Add App and Notes component

---
+++ app/components/Notes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default ({notes}) => (
+    <ul>
+        {
+            notes.map(
+                note =>
+                    <li key = {note.id} > {note.task}</li>
+            )
+        }
+    </ul>
+)
+
